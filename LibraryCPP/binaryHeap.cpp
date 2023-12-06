69ff6c1a0a3abbc83c324024115caa89be8479e2
Sergey Antonov <59932798+Sergeo-Sss@users.noreply.github.com>
2023-12-06 20:23:18 +0300

    add comments

--- LibraryCPP/binaryHeap.cpp
+++ LibraryCPP/binaryHeap.cpp
@@ -2,11 +2,11 @@
 
 struct BinaryHeap
 {
-    void** heapData;
-    size_t heapSize;
-    size_t dataCount;
-    Comparator compare;
-    Destructor destroy;
+    void** heapData;    // Массив указателей для хранения элементов кучи
+    size_t heapSize;    // Максимальное количество элементов, которое может хранить куча
+    size_t dataCount;   // Текущее количество элементов в куче
+    Comparator compare; // Функция-компаратор для сравнения элементов кучи
+    Destructor destroy; // Функция-деструктор для удаления элементов
 };
 
 BinaryHeap* binaryHeap_create(const size_t size, Comparator comp, Destructor dest)
@@ -29,52 +29,82 @@ void binaryHeap_swapData(BinaryHeap* heap, const size_t firstIndex, const size_t
 
 void binaryHeap_heapify(BinaryHeap* heap, int i)
 {
-    int left = 2 * i + 1;
-    int right = 2 * i + 2;
-    int smallest = i;
+    // Вычисляем индексы левого и правого дочерних элементов для данного узла
+    int left = 2 * i + 1;  // Индекс левого дочернего элемента
+    int right = 2 * i + 2; // Индекс правого дочернего элемента
+    int smallest = i;      // Индекс наименьшего элемента, начинаем с текущего узла
 
+    // Проверяем, меньше ли левый дочерний элемент текущего узла
+    // и находится ли он в пределах размера кучи
     if (left < (int)heap->dataCount && heap->compare(heap->heapData[left], heap->heapData[smallest]) < 0)
         smallest = left;
+
+    // Аналогично проверяем правый дочерний элемент
     if (right < (int)heap->dataCount && heap->compare(heap->heapData[right], heap->heapData[smallest]) < 0)
         smallest = right;
+
+    // Если один из дочерних элементов меньше текущего элемента, меняем их местами
     if (smallest != i)
     {
         binaryHeap_swapData(heap, i, smallest);
+        // Рекурсивно применяем heapify к измененному дочернему элементу
         binaryHeap_heapify(heap, smallest);
     }
 }
 
 void binaryHeap_insert(BinaryHeap* heap, void* node)
 {
+    // Проверяем, не достигнут ли максимальный размер кучи.
+    // Если достигнут, то новый элемент добавить нельзя.
     if (heap->dataCount == heap->heapSize)
         return;
 
+    // Увеличиваем количество элементов в куче на единицу.
     heap->dataCount++;
+    // Рассчитываем индекс, где будет размещён новый элемент.
     size_t i = heap->dataCount - 1;
+    // Размещаем новый элемент в конец массива кучи.
     heap->heapData[i] = node;
 
+    // Проходим циклом вверх по куче для восстановления её свойств.
+    // Пока не достигнут корень кучи и пока родительский элемент больше нового элемента...
     while (i != 0 && heap->compare(heap->heapData[(i - 1) / 2], heap->heapData[i]) > 0)
     {
+        // Обмениваем новый элемент с его родителем.
         binaryHeap_swapData(heap, (i - 1) / 2, i);
+        // Перемещаем индекс на позицию родительского элемента.
         i = (i - 1) / 2;
     }
 }
 
 void* binaryHeap_extractMin(BinaryHeap* heap)
 {
+    // Проверяем, есть ли элементы в куче. Если нет, возвращаем NULL.
     if (heap->dataCount <= 0)
         return NULL;
+
+    // Если в куче только один элемент, уменьшаем количество элементов
+    // и возвращаем этот единственный элемент.
     if (heap->dataCount == 1)
     {
         heap->dataCount--;
         return heap->heapData[0];
     }
 
+    // Сохраняем корневой элемент (минимальный элемент кучи) для последующего возвращения.
     void* root = heap->heapData[0];
+
+    // Заменяем корневой элемент последним элементом в куче.
     heap->heapData[0] = heap->heapData[heap->dataCount - 1];
+
+    // Уменьшаем количество элементов в куче на единицу.
     heap->dataCount--;
+
+    // Применяем процедуру "heapify" начиная с корня, чтобы восстановить свойства кучи
+    // и убедиться, что все родительские элементы меньше своих дочерних.
     binaryHeap_heapify(heap, 0);
 
+    // Возвращаем изначальный корневой элемент, который является минимальным элементом кучи.
     return root;
 }
 
--- LibraryCPP/huffmanTree.cpp
+++ LibraryCPP/huffmanTree.cpp
@@ -1,15 +1,17 @@
 #include "huffmanTree.h"
 
+// Структура узла дерева Хаффмана
 struct HuffmanNode
 {
-    bool isLeaf;
-    unsigned long long int weight;
-    unsigned char symbol;
+    bool isLeaf; // Флаг, указывающий, является ли узел листом
+    unsigned long long int weight; // Вес узла, используется для построения дерева
+    unsigned char symbol; // Символ, соответствующий узлу (только для листьев)
 
-    HuffmanNode* leftNode;
-    HuffmanNode* rightNode;
+    HuffmanNode* leftNode; // Указатель на левый дочерний узел
+    HuffmanNode* rightNode; // Указатель на правый дочерний узел
 };
 
+// Функция для создания листового узла
 HuffmanNode* huffman_createLeafNode(unsigned char symbol, unsigned long long int weight)
 {
     HuffmanNode* node = new HuffmanNode;
@@ -21,6 +23,7 @@ HuffmanNode* huffman_createLeafNode(unsigned char symbol, unsigned long long int
     return node;
 }
 
+// Функция для создания внутреннего узла
 HuffmanNode* huffman_createInternalNode(HuffmanNode* leftNode, HuffmanNode* rightNode)
 {
     HuffmanNode* node = new HuffmanNode;
@@ -31,52 +34,61 @@ HuffmanNode* huffman_createInternalNode(HuffmanNode* leftNode, HuffmanNode* righ
     return node;
 }
 
+// Получение левого дочернего узла
 HuffmanNode* huffman_getLeftNode(HuffmanNode* node)
 {
     return node ? node->leftNode : NULL;
 }
 
+// Получение правого дочернего узла
 HuffmanNode* huffman_getRightNode(HuffmanNode* node)
 {
     return node ? node->rightNode : NULL;
 }
 
+// Установка левого дочернего узла
 void huffman_setLeftNode(HuffmanNode* node, HuffmanNode* newNode)
 {
     node->leftNode = node->leftNode ? node->leftNode : newNode;
 }
 
+// Установка правого дочернего узла
 void huffman_setRightNode(HuffmanNode* node, HuffmanNode* newNode)
 {
     node->rightNode = node->rightNode ? node->rightNode : newNode;
 }
 
+// Проверка, является ли узел листом
 bool huffman_nodeIsLeaf(HuffmanNode* node)
 {
     return node->isLeaf;
 }
 
+// Получение символа из узла
 unsigned char huffman_getNodeChar(HuffmanNode* node)
 {
     return node->symbol;
 }
 
+// Получение веса узла
 unsigned long long int huffman_getNodeWeight(HuffmanNode* node)
 {
     return node->weight;
 }
 
+// Удаление дерева Хаффмана (рекурсивная функция)
 HuffmanNode* huffman_deleteTree(HuffmanNode* node)
 {
     if (node)
     {
-        huffman_deleteTree(node->leftNode);
-        huffman_deleteTree(node->rightNode);
-        delete node;
+        huffman_deleteTree(node->leftNode); // Удаление левого поддерева
+        huffman_deleteTree(node->rightNode); // Удаление правого поддерева
+        delete node; // Удаление текущего узла
     }
     return nullptr;
 }
 
+// Компаратор для узлов (используется в очереди с приоритетом)
 int huffmanNodeComparator(const void* nodeA, const void* nodeB)
 {
     HuffmanNode* huffmanNodeA = (HuffmanNode*)nodeA;
@@ -89,24 +101,26 @@ int huffmanNodeComparator(const void* nodeA, const void* nodeB)
     return 0;
 }
 
+// Деструктор для узлов (используется в очереди с приоритетом)
 void huffmanNodeDestructor(void* node)
 {
     HuffmanNode* huffmanNode = (HuffmanNode*)node;
     huffman_deleteTree(huffmanNode);
 }
 
+// Функция для печати дерева Хаффмана (для отладки)
 void huffman_printTree(HuffmanNode* node, unsigned int height)
 {
     if (node)
     {
-        huffman_printTree(node->rightNode, height + 1);
-        for (unsigned int i = 0; i < height; i++)
+        huffman_printTree(node->rightNode, height + 1); // Печать правого поддерева
+        for (unsigned int i = 0; i < height; i++) // Отступ для визуализации уровня
             std::cout << "  ";
         if (node->isLeaf)
-            std::cout << (unsigned char)node->symbol;
+            std::cout << (unsigned char)node->symbol; // Печать символа листа
         else
-            std::cout << node->weight;
-        huffman_printTree(node->leftNode, height + 1);
+            std::cout << node->weight; // Печать веса внутреннего узла
+        huffman_printTree(node->leftNode, height + 1); // Печать левого поддерева
     }
     else
         std::cout << std::endl;
